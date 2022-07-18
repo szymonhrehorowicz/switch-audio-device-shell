c6ee6f7c7158dd79b33fcd0e3bcdaa70548ceb1c
szymonhrehorowicz <szymhre@gmail.com>
2022-07-18 15:46:38 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,20 @@
+It's a simple shell script that let's you switch your current audio output device.
+
+##### To use it, you have to install [NirCmd](https://www.nirsoft.net/utils/nircmd.html 'NirCmd').
+
+##### How to use?
+
+-   With `s` parameter you can order the script to change your output audio device.
+
+    ###### Example
+
+    `chaudio s`
+
+#
+
+-   With `d1`(`d2`) parameter followed by name of the audio device closed in quotation marks, you can change the name of the first(second) audio device in your config file.
+
+    ###### Example
+
+    `chaudio d1 "Headphones"`
+    `chaudio d2 "Speakers"`
---
+++ chaudio.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+CONFIG_FILE="./config.conf"
+DEVICE_1=""
+DEVICE_2=""
+CURR_OUT=""
+
+# Load initial setup from config file
+while read parameter sign value
+do
+    case $parameter in
+     DEVICE_1)
+          DEVICE_1=$value
+          ;;
+     DEVICE_2)
+          DEVICE_2=$value
+          ;;
+     CURR_OUT)
+          CURR_OUT=$value
+          ;;
+    esac
+    
+done < $CONFIG_FILE
+
+# Switch audio device with "s" parameter
+if [ "$1" == "s" ]
+then
+    if [ $CURR_OUT == 0 ]
+    then
+        CURR_OUT=1
+        nircmd setdefaultsounddevice "$DEVICE_2" 1
+    else
+        CURR_OUT=0
+        nircmd setdefaultsounddevice "$DEVICE_1" 1
+    fi
+fi
+
+# Change the name of DEVICE_1
+if [ "$1" = "d1" ]
+then
+    DEVICE_1=$2
+fi
+
+# Change the name of DEVICE_2
+if [ "$1" = "d2" ]
+then
+    DEVICE_2=$2
+fi
+
+# Save new config
+echo "" > config.conf
+echo "DEVICE_1 = $DEVICE_1" >> config.conf
+echo "DEVICE_2 = $DEVICE_2" >> config.conf
+echo "CURR_OUT = $CURR_OUT" >> config.conf
